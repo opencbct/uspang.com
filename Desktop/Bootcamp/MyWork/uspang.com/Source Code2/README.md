04f7f5362810e28ae0b8f151f4705218e8c2e120
Benjamin Kim <84877261+opencbct@users.noreply.github.com>
2022-01-09 12:30:07 -0600

    updated

---
+++ Desktop/Bootcamp/MyWork/uspang.com/Source Code2/README.md
@@ -0,0 +1,5 @@
+If you host this website only as a static website you don't need node JS.
+
+But if you want to run this in your localhost. Make sure to run ` npm install ` command in your terminal to install all the packages.
+
+After done installing, you'll be able to run the website on localhost by ` npm start ` command.
---
+++ Desktop/Bootcamp/MyWork/uspang.com/Source Code4/public/project.js
@@ -0,0 +1,52 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #ui/ux, #backend",
+        image: "project (1).png",
+    },
+    {
+        name: "project two",
+        tags: "#javascript, #fullstack",
+        image: "project (2).png",
+    },
+    {
+        name: "project three",
+        tags: "#ui/ux",
+        image: "project (3).png",
+    },
+    {
+        name: "project four",
+        tags: "#ui/ux, #backend",
+        image: "project (4).png",
+    },
+    {
+        name: "project five",
+        tags: "#fullstack, #backend",
+        image: "project (5).png",
+    },
+    {
+        name: "project six",
+        tags: "#javascript, #ui/ux, #backend",
+        image: "project (6).png",
+    },
+    {
+        name: "project seven",
+        tags: "#fullstack",
+        image: "project (7).png",
+    },
+    {
+        name: "project eight",
+        tags: "#javascript",
+        image: "project (8).png",
+    },
+    {
+        name: "project nine",
+        tags: "#backend",
+        image: "project (9).png",
+    },
+    {
+        name: "project ten",
+        tags: "#javascript, #ui/ux",
+        image: "project (10).png",
+    }
+]
